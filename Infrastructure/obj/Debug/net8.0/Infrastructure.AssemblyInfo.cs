6780c52a915c455083074f44d553683f5f7458d0
karimmoustamid <moustamid.teck@gmail.com>
2024-11-30 12:44:44 -0600

    Remove Swagger setup from the API project.
    The commit eliminates the Swashbuckle.AspNetCore package and its dependencies from the API project. The Swagger configuration has been removed from Program.cs and the related package references have been deleted from API.csproj. Additionally, changes were made to logging settings in appsettings.Development.json to align with the project's updated needs.

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8655a1a089aa4b8c23a3307f6e3af0ecb01a95b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34df80508a7997eb25a95484d0284065dde62e2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
