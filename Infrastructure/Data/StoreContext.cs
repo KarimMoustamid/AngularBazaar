c42d837f07794f00960fab16ad33281cd1125cd1
karimmoustamid <moustamid.teck@gmail.com>
2024-12-02 17:47:57 -0600

    Add DbContext configuration to StoreContext
    Introduced OnModelCreating in StoreContext to apply configurations from assembly using EF Core. Updated dependencies and configuration files to support new changes and ensure correct entity framework functioning with SQL server. Various project files were modified due to the build process and dependency changes.

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,10 +1,17 @@
 namespace Infrastructure.Data
 {
+    using config;
     using Core.Entities;
     using Microsoft.EntityFrameworkCore;
 
     public class StoreContext(DbContextOptions<StoreContext> options) : DbContext(options)
     {
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
     }
 }
---
+++ Infrastructure/config/ProductConfiguration.cs
@@ -0,0 +1,14 @@
+namespace Infrastructure.config
+{
+    using Core.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
