26c2a3acb8a68c8aa46bddb14a73a0feb6fe85dd
karimmoustamid <moustamid.teck@gmail.com>
2024-12-03 11:33:08 -0600

    Remove unwanted files

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9284c72c1ec1a21c0a641b9e1719d7193544373b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6580ad09740c594e851c7996ce21aa046be49ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
